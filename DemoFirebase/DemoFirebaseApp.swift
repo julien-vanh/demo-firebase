6011a19385d1a2d17e35a4bdb8b6eb97363193fd
Julien Vanheule <julien.vanheule@aunis.studio>
2024-02-04 21:36:09 +0100

    Initial Commit

---
+++ DemoFirebase/DemoFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoFirebaseApp.swift
+//  DemoFirebase
+//
+//  Created by Julien Vanheule on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct DemoFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
